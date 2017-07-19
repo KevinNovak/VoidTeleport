8fe4a5728c960cc9cc3faf23243fb8a7ccb8d8cf
Kevin Novak <kevinmnovak@yahoo.com>
2017-07-18 22:36:52 -0400

    Working on VoidWorld class

--- src/me/kevinnovak/voidteleport/VoidWorld.java
+++ src/me/kevinnovak/voidteleport/VoidWorld.java
@@ -1,6 +1,17 @@
 package me.kevinnovak.voidteleport;
 
+import java.util.List;
+
 public class VoidWorld {
+	boolean enabled;
+	String voidSpawnWorld;
+	int voidSpawnXPos, voidSpawnYPos, voidSpawnZPos;
+	
+	boolean voidRandomSpawnEnabled;
+	String voidRandomSpawnWorld;
+	int voidRandomSpawnXRangeMin, voidRandomSpawnXRangeMax, voidRandomSpawnZRangeMin, voidRandomSpawnZRangeMax;
+	
+	List<String> dontSpawnOn;
 	
 	VoidWorld() {
 		

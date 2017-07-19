19dd3bfcc9680d35c0582f759cacc77d2372ecbd
Kevin Novak <kevinmnovak@yahoo.com>
2017-07-18 22:40:46 -0400

    Created a VoidWorldManager

---
+++ src/me/kevinnovak/voidteleport/VoidWorldManager.java
@@ -0,0 +1,13 @@
+package me.kevinnovak.voidteleport;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class VoidWorldManager {
+	List<VoidWorld> voidWorlds = new ArrayList<VoidWorld>();
+	
+	VoidWorldManager() {
+		
+    }
+	
+}

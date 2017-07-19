b22b8ba440ec9669c6045afb64a700847dba9c15
Kevin Novak <kevinmnovak@yahoo.com>
2017-07-19 11:11:17 -0400

    Getters and setters for VoidWorld

--- src/me/kevinnovak/voidteleport/VoidWorld.java
+++ src/me/kevinnovak/voidteleport/VoidWorld.java
@@ -2,11 +2,11 @@ package me.kevinnovak.voidteleport;
 
 import java.util.List;
 
+import org.bukkit.Location;
+
 public class VoidWorld {
-	boolean enabled;
-	String voidSpawnWorld;
-	int voidSpawnXPos, voidSpawnYPos, voidSpawnZPos;
-	
+	String name;
+	Location spawnLocation;
 	boolean voidRandomSpawnEnabled;
 	String voidRandomSpawnWorld;
 	int voidRandomSpawnXRangeMin, voidRandomSpawnXRangeMax, voidRandomSpawnZRangeMin, voidRandomSpawnZRangeMax;
@@ -17,4 +17,67 @@ public class VoidWorld {
 		
     }
 	
+	String getName() {
+		return this.name;
+	}
+	
+	void setName(String name) {
+		this.name = name;
+	}
+	
+	Location getSpawnLocation() {
+		return this.spawnLocation;
+	}
+	
+	void setSpawnLocation(Location spawnLocation) {
+		this.spawnLocation = spawnLocation;
+	}
+	
+	boolean getVoidRandomSpawnEnabled() {
+		return this.voidRandomSpawnEnabled;
+	}
+	
+	void setVoidRandomSpawnEnabled(boolean voidRandomSpawnEnabled) {
+		this.voidRandomSpawnEnabled = voidRandomSpawnEnabled;
+	}
+	
+	String getVoidRandomSpawnWorld() {
+		return this.voidRandomSpawnWorld;
+	}
+	
+	void setRandomSpawnWorld(String voidRandomSpawnWorld) {
+		this.voidRandomSpawnWorld = voidRandomSpawnWorld;
+	}
+	
+	int getVoidRandomSpawnXRangeMin() {
+		return this.voidRandomSpawnXRangeMin;
+	}
+	
+	void setVoidRandomSpawnXRangeMin(int voidRandomSpawnXRangeMin) {
+		this.voidRandomSpawnXRangeMin = voidRandomSpawnXRangeMin;
+	}
+	
+	int getVoidRandomSpawnXRangeMax() {
+		return this.voidRandomSpawnXRangeMax;
+	}
+	
+	void setVoidRandomSpawnXRangeMax(int voidRandomSpawnXRangeMax) {
+		this.voidRandomSpawnXRangeMax = voidRandomSpawnXRangeMax;
+	}
+	
+	int getVoidRandomSpawnZRangeMin() {
+		return this.voidRandomSpawnZRangeMin;
+	}
+	
+	void setVoidRandomSpawnZRangeMin(int voidRandomSpawnZRangeMin) {
+		this.voidRandomSpawnZRangeMin = voidRandomSpawnZRangeMin;
+	}
+	
+	int getVoidRandomSpawnZRangeMax() {
+		return this.voidRandomSpawnZRangeMax;
+	}
+	
+	void setVoidRandomSpawnZRangeMax(int voidRandomSpawnZRangeMax) {
+		this.voidRandomSpawnZRangeMax = voidRandomSpawnZRangeMax;
+	}
 }
